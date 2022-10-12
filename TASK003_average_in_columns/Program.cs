dc9af556306fd155c6accd3fb55b2abb8d6fbd88
Victor Rusaev <rusaev74@mail.ru>
2022-10-12 20:20:49 +0300

    TASK003 average in columns

---
+++ TASK003_average_in_columns/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Clear();
+
+int[,] matrix = new int[3, 4];
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+CalcAverageInColumns(matrix);
+
+void CalcAverageInColumns(int[,] matr, double sum = 0, int counter = 0)
+{
+    double[] averageArray = new double[matr.GetLength(1)];
+
+    while(counter < matr.GetLength(1))
+    {
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            sum = sum + matr[i, counter];
+        }
+        averageArray[counter] = Math.Round(sum / matr.GetLength(0), 2);
+        Console.Write($"{averageArray[counter]}; ");
+        counter++;
+        sum = 0;
+    }
+    
+}    
+
+void FillArray(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
